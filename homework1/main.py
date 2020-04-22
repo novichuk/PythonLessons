13303b77f8f476ca017744b8d221f36f3047cfd4
novichuk <octavemaster@gmail.com>
2020-04-23 02:49:33 +0300

    implemented parse_cookie function

--- homework1/main.py
+++ homework1/main.py
@@ -30,3 +30,37 @@ if __name__ == '__main__':
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
+
+def parse_cookie(query: str) -> dict:
+    result = {}
+    # print(query.index('=')) #4 posle 4 splituem
+
+    #  check if empty
+    if not query:
+        return result
+
+    # check '=' not exist
+    if not '=' in query:
+        return result
+
+    # split by key-value blocks
+    obj = query.split(';')
+
+    # main logic: output in cookie format
+    for i in obj:
+        # skip if empty element
+        if not i:
+            continue
+        # get num of element of '=' and split by 2 parts
+        equal_elem_num = i.index('=')
+        result.update({i[:equal_elem_num]: i[equal_elem_num+1:]})
+    return result
+
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+
