10365783915fc33f5c8d5168aaf89c1ab146abb9
novichuk <octavemaster@gmail.com>
2020-05-02 14:15:59 +0300

    homework2

--- homework2/req.py
+++
@@ -1,6 +0,0 @@
-def read_file(path) -> str:
-    # path = '../requirements.txt'
-    requirements_file = open(path, 'r')
-    requirements = requirements_file.read()
-    requirements_file.close()
-    return requirements
