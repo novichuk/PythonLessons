6affe52e3ce61dbcdcc9554c241f495b7e99e59d
novichuk <octavemaster@gmail.com>
2020-05-02 13:22:37 +0300

    cleaning

--- homework2/space.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-
-def num_of_astronauts() -> str:
-    r = requests.get('http://api.open-notify.org/astros.json')
-    result = json.loads(r.text)
-    result = str(result['number'])
-    return result
