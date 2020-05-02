493a146205ff43c0e0c990e8ed97cfed797ef16f
novichuk <octavemaster@gmail.com>
2020-05-02 14:25:44 +0300

    clear

--- homework2/gen_users.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-
-
-def generate_users(amount):
-    fake = Faker()
-    result = {}
-    for i in range(1, amount + 1):
-        result[i] = f'{fake.name()}, {fake.email()}'
-    return result
